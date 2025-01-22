0566947292ba2108701479007e524f1f341f7d69
Vladimir Starkov <iamstarkov@gmail.com>
2025-01-22 15:16:27 +0100

    Update demo.js to include custom green function and modify console log example

--- demo.js
+++ demo.js
@@ -1,3 +1,7 @@
 import styles from './index.js';
 
-console.log(`${styles.green.open}Hello world!${styles.green.close}`);
+console.log(`${styles.green.open} styles.green ${styles.green.close}`);
+
+function green(str) { return `\u001B[32m${str}\u001B[39m` }
+
+console.log(green('custom green'));
